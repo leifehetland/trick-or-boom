6623bb6fbd2adf0701e2594708246350fcefff96
Leif Hetland <lehetland1@gmail.com>
2016-06-14 01:22:00 -0500

    instantiates bomb radius and creates explosion animation

--- Trick-Or-Boom/Scripts/phaserJS/prefabs/Bomb.js
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/Bomb.js
@@ -1,1 +1,15 @@
-﻿
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.Bomb = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Prefab.call(this, game_state, name, position, properties);
+
+    this.anchor.setTo(0.5);
+
+    this.bomb_radius = +properties.bomb_radius;
+
+    this.game_state.game.physics.arcade.enable(this);
+    this.body.immovable = true;
+
+    this.exploding_animation = this.animations.add("exploding", [0, 2, 4], 1, false);
+}
