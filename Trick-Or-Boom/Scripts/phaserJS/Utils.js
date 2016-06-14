20260a58d103b6d9d8d523550f592451f261786b
Leif Hetland <lehetland1@gmail.com>
2016-06-14 01:28:00 -0500

    allows for all the prefab js files to be used over and over

--- Trick-Or-Boom/Scripts/phaserJS/Utils.js
+++ Trick-Or-Boom/Scripts/phaserJS/Utils.js
@@ -3,6 +3,7 @@
 TrickOrBoom.create_prefab_from_pool = function (pool, prefab_constructor, game_state, prefab_name, prefab_position, prefab_properties) {
     "use strict";
     var prefab;
+
     prefab = pool.getFirstDead();
     if (!prefab) {
         prefab = new prefab_constructor(game_state, prefab_name, prefab_position, prefab_properties);
