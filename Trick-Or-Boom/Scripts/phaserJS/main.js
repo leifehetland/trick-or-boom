8ea79948c4a5b7761dd9541f546746c22356fac6
Leif Hetland <lehetland1@gmail.com>
2016-06-14 01:00:59 -0500

    transfered artwork to Windows as well as main.js

--- Trick-Or-Boom/Scripts/phaserJS/main.js
+++ Trick-Or-Boom/Scripts/phaserJS/main.js
@@ -4,4 +4,4 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS);
 game.state.add("BootState", new TrickOrBoom.BootState());
 game.state.add("LoadingState", new TrickOrBoom.LoadingState());
 game.state.add("TiledState", new TrickOrBoom.TiledState());
-game.state.start("BootState", true, false, "~/Assets/levels/level1.json", "TiledState")
+game.state.start("BootState", true, false, "Assets/levels/level1.json", "TiledState");
