18b8536fe96f71536fa5926de39f1cf3627b357c
Leif Hetland <lehetland1@gmail.com>
2016-06-16 00:54:46 -0500

    updated main js to start with title screen instead of jumping right into the game

--- Trick-Or-Boom/Scripts/phaserJS/main.js
+++ Trick-Or-Boom/Scripts/phaserJS/main.js
@@ -1,7 +1,8 @@
 ﻿var TrickOrBoom = TrickOrBoom || {};
 
-var game = new Phaser.Game(640, 480, Phaser.CANVAS);
+var game = new Phaser.Game(400, 400, Phaser.CANVAS);
 game.state.add("BootState", new TrickOrBoom.BootState());
 game.state.add("LoadingState", new TrickOrBoom.LoadingState());
-game.state.add("TiledState", new TrickOrBoom.TiledState());
-game.state.start("BootState", true, false, "Assets/levels/level1.json", "TiledState");
+game.state.add("TitleState", new TrickOrBoom.TitleState());
+game.state.add("StartGameState", new TrickOrBoom.StartGameState());
+game.state.start("BootState", true, false, "assets/levels/title_screen.json", "TitleState");
