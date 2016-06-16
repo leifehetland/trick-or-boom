44fb5774afe070f3aad7bf2ab2c862c0911dbbc7
Leif Hetland <lehetland1@gmail.com>
2016-06-16 00:39:18 -0500

    if all 3 targets are destroyed it generates a gate which is the goal

---
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/Goal.js
@@ -0,0 +1,25 @@
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.Goal = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Prefab.call(this, game_state, name, position, properties);
+
+    this.anchor.setTo(0.5);
+    this.scale.setTo(0.5);
+
+    this.game_state.game.physics.arcade.enable(this);
+    this.body.immovable = true;
+};
+
+TrickOrBoom.Goal.prototype = Object.create(TrickOrBoom.Prefab.prototype);
+TrickOrBoom.Goal.prototype.constructor = TrickOrBoom.Goal;
+
+TrickOrBoom.Goal.prototype.update = function () {
+    "use strict";
+    this.game_state.game.physics.arcade.overlap(this, this.game_state.groups.players, this.reach_goal, null, this);
+};
+
+TrickOrBoom.Goal.prototype.reach_goal = function () {
+    "use strict";
+    this.game_state.next_level();
+};
---
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/Target.js
@@ -0,0 +1,30 @@
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.Target = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Prefab.call(this, game_state, name, position, properties);
+
+    this.anchor.setTo(0.5);
+
+    this.game_state.game.physics.arcade.enable(this);
+    this.body.immovable = true;
+};
+
+TrickOrBoom.Target.prototype = Object.create(TrickOrBoom.Prefab.prototype);
+TrickOrBoom.Target.prototype.constructor = TrickOrBoom.Target;
+
+TrickOrBoom.Target.prototype.update = function () {
+    "use strict";
+    this.game_state.game.physics.arcade.overlap(this, this.game_state.groups.explosions, this.kill, null, this);
+};
+
+TrickOrBoom.Target.prototype.kill = function () {
+    "use strict";
+    var goal_position, goal_properties, goal;
+    Phaser.Sprite.prototype.kill.call(this);
+    if (this.game_state.groups.targets.countLiving() === 0) {
+        goal_position = new Phaser.Point(this.game_state.game.world.width / 2, this.game_state.game.world.height / 2);
+        goal_properties = { texture: "goal_image", group: "goals" };
+        goal = new TrickOrBoom.Goal(this.game_state, "goal", goal_position, goal_properties);
+    }
+};
