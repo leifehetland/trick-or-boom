4c86bda73680b39acc8755e381ae6c2fb037ba24
Leif Hetland <lehetland1@gmail.com>
2016-06-16 00:45:21 -0500

    sets up the item collector and adds extra life if picked up

---
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/Item.js
@@ -0,0 +1,26 @@
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.Item = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Prefab.call(this, game_state, name, position, properties);
+
+    this.anchor.setTo(0.5);
+
+    this.game_state.game.physics.arcade.enable(this);
+    this.body.immovable = true;
+
+    this.scale.setTo(0.75);
+};
+
+TrickOrBoom.Item.prototype = Object.create(TrickOrBoom.Prefab.prototype);
+TrickOrBoom.Item.prototype.constructor = TrickOrBoom.Item;
+
+TrickOrBoom.Item.prototype.update = function () {
+    "use strict";
+    this.game_state.game.physics.arcade.overlap(this, this.game_state.groups.players, this.collect_item, null, this);
+};
+
+TrickOrBoom.Item.prototype.collect_item = function () {
+    "use strict";
+    this.kill();
+};
---
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/LifeItem.js
@@ -0,0 +1,15 @@
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.LifeItem = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Item.call(this, game_state, name, position, properties);
+};
+
+TrickOrBoom.LifeItem.prototype = Object.create(TrickOrBoom.Item.prototype);
+TrickOrBoom.LifeItem.prototype.constructor = TrickOrBoom.LifeItem;
+
+TrickOrBoom.LifeItem.prototype.collect_item = function (item, player) {
+    "use strict";
+    TrickOrBoom.Item.prototype.collect_item.call(this);
+    player.number_of_lives += 1;
+};
