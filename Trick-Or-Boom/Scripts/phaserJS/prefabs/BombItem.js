02c88ccf9813c6a6a30a9792e9e5cf18539fed38
Leif Hetland <lehetland1@gmail.com>
2016-06-16 00:29:30 -0500

    updated scripts and Bomb Item makes sure even with power ups only 5 bombs can be laid at a time

---
+++ Trick-Or-Boom/Scripts/phaserJS/prefabs/BombItem.js
@@ -0,0 +1,17 @@
+﻿var TrickOrBoom = TrickOrBoom || {};
+
+TrickOrBoom.BombItem = function (game_state, name, position, properties) {
+    "use strict";
+    TrickOrBoom.Item.call(this, game_state, name, position, properties);
+
+    this.MAXIMUM_NUMBER_OF_BOMBS = 5;
+};
+
+TrickOrBoom.BombItem.prototype = Object.create(TrickOrBoom.Item.prototype);
+TrickOrBoom.BombItem.prototype.constructor = TrickOrBoom.BombItem;
+
+TrickOrBoom.BombItem.prototype.collect_item = function (item, player) {
+    "use strict";
+    TrickOrBoom.Item.prototype.collect_item.call(this);
+    player.number_of_bombs = Math.min(player.number_of_bombs + 1, this.MAXIMUM_NUMBER_OF_BOMBS);
+};
